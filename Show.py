56e896ebd952a7fb0741d149b57219c5c30de560
PhoenixPars <60186819+PhoenixPars@users.noreply.github.com>
2023-08-24 06:54:49 +0330

    Create Show.py
    Create tool

---
+++ Show.py
@@ -0,0 +1,9 @@
+#PhoenixPars
+import socket
+Host_Name = socket.gethostname()
+My_IPAddress = socket.gethostbyname(Host_Name)
+print("Your Host name : {Host_Name}")
+print("Your IP Address: {My_IPAddress}")
+print("——————————————————————————————————————")
+print("Creator : PhoenixPars")
+print("My Telegram channel : t.me/PhoenixPars")
