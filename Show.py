1fd5b1e41fb9f45f6c328ebe7019f51cefa823fc
PhoenixPars <60186819+PhoenixPars@users.noreply.github.com>
2023-08-24 07:18:27 +0330

    Update Show.py

--- Show.py
+++ Show.py
@@ -2,8 +2,8 @@
 import socket
 Host_Name = socket.gethostname()
 My_IPAddress = socket.gethostbyname(Host_Name)
-print(f"Your Host name : {Host_Name}")
-print(f"Your IP Address: {My_IPAddress}")
+print("Your Host name : " + Host_Name)
+print("Your IP Address: " + My_IPAddress)
 print("——————————————————————————————————————")
 print("Creator : PhoenixPars")
 print("My Telegram channel : t.me/PhoenixPars")
